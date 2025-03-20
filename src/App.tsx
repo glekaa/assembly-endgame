31561b8945de862dbfeab3c5df7fb36b9f92da3b
Pavel Melnik <pan4kkpan4@gmail.com>
2025-03-20 14:50:28 +0100

    feat: add alert section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import './App.css'
 
 function App() {
-
-  return (
-      <main>
-          <header>
-              <h1>Assembly: Endgame</h1>
-              <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
-          </header>
-      </main>
-  )
+    /**
+     * Goal: Build out the main parts of our app
+     *
+     * Challenge: Build a status section below the header.
+     * For now, you can just hard-code in the styles for
+     * a winning game, and we'll make it more dynamic
+     * later.
+     */
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="alert">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+        </main>
+    )
 }
 
 export default App
